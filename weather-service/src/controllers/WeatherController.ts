f714d17e130583cc4ec1198b9cc4e995a8d88715
Roberto Sousa <beto.sousa22@gmail.com>
2025-07-25 01:26:33 +0100

    Enhance WeatherController to require city query parameter and update fetchWeather method to retrieve weather data for specified city

--- weather-service/src/controllers/WeatherController.ts
+++ weather-service/src/controllers/WeatherController.ts
@@ -4,8 +4,14 @@ import { WeatherService } from '../services/WeatherService';
 export class WeatherController { // do not control the weather :-D
     constructor(private weatherService: WeatherService) { }
 
-    async fetchWeather(_req: Request, res: Response) {
-        const data = await this.weatherService.fetchWeather();
+    async fetchWeather(req: Request, res: Response) {
+
+        const city = req.query.city as string;  // <-- Grab the query parameter
+
+        if (!city) {
+            return res.status(400).json({ error: 'City is required' });
+        }
+        const data = await this.weatherService.getWeather(city);
         res.json(data);
     }
 }
