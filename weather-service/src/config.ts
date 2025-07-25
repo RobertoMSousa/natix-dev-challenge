69abb16c5236aca34c9053a141d1d1ea0ed7d388
Roberto Sousa <beto.sousa22@gmail.com>
2025-07-25 01:25:55 +0100

    Add additional configuration variables for API and Redis in weather service

--- weather-service/src/config.ts
+++ weather-service/src/config.ts
@@ -1,2 +1,6 @@
 export const environment = process.env.NODE_ENV;
 export const port = process.env.PORT || 4000;
+export const apiUri = process.env.API_URI;
+export const apiKey = process.env.API_KEY;
+export const redisUri = process.env.REDIS_URI;
+export const redisPort = process.env.REDIS_PORT || "6379";
